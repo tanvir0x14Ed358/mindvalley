b4c5a2c332bbde7ef597426569e0bc6cfaab5fff
Tanvir Alam <alam1674@gmail.com>
2019-01-20 06:39:08 +0600

    fixed error sessionGuard:factory method not found caused by guard()

--- medium/app/Http/Controllers/AuthController.php
+++ medium/app/Http/Controllers/AuthController.php
@@ -25,6 +25,7 @@ class AuthController extends Controller
         $credentials = $request->only(['email', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
+            var_dump($credentials);die();
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -36,7 +37,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
 }
